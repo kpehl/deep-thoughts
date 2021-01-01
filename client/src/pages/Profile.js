33b7ef9c8c918fed4111a4e502ab7895bfe1dc2f
Kathleen Pehl <kathleen.pehl@gmail.com>
2021-01-01 12:54:21 -0500

    added ThoughtForm component and imported it into Home and Profile pages for a logged in user; added character limit and visual indication of reaching limit; added function to clear form when submit button is hit

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,7 @@ import Auth from '../utils/auth';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
@@ -71,6 +72,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
