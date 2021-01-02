4f7093f711d5aec99f85d0fab4316ba78a728ff3
Kathleen Pehl <kathleen.pehl@gmail.com>
2021-01-01 20:51:42 -0500

    added ReactionForm component and imported into SingleThought page; verified that form renders

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
+import Auth from '../utils/auth'
 import ReactionList from '../components/ReactionList';
 import { Link } from 'react-router-dom';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -36,6 +38,7 @@ const SingleThought = props => {
           </div>
         </div>
         {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+        {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
       </div>
   );
 };
